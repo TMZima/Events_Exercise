cbd92e5c61a05dea44f3953a0e151792449501f3
Trey Zima <trey.m.zima@gmail.com>
2025-01-05 11:39:26 -0700

    Added a listener for when the color form is submitted it will update the color of all the boxes and reset the input field

--- script.js
+++ script.js
@@ -6,4 +6,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
   let boxColor = null; // Stores the color for boxes submitted from the form
   let boxId = 0; // Counter for assigning ID to boxes
+
+  colorForm.addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevents default behavior of refreshing the page
+
+    const newBoxColor = colorInput.value;
+    const boxes = document.querySelectorAll(".box");
+
+    for (let box of boxes) {
+      box.style.backgoundColor = newBoxColor;
+    }
+
+    colorInput.value = ""; // Resets the input field after it's submitted
+    boxColor = newBoxColor; // Sets the box color to the new color that's submitted
+  });
 });
