c7d1eeb1213520e0294df4c911644d298bb23456
Trey Zima <trey.m.zima@gmail.com>
2025-01-05 11:02:42 -0700

    Created listener on DOM content being loaded, selected the elements for exercise, and created variables to store box color and box ID

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const boxContainer = document.querySelector("#box-container");
+  const newBoxBtn = document.querySelector("#new-box-button");
+  const colorForm = document.querySelector("#color-form");
+  const colorInput = document.querySelector("#color-input");
+
+  let boxColor = null; // Stores the color for boxes submitted from the form
+  let boxId = 0; // Counter for assigning ID to boxes
+});
