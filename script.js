0eb309a9c66184d19fb8628f713afff7863971a1
Trey Zima <trey.m.zima@gmail.com>
2025-01-05 12:09:25 -0700

    Created a function to add a new box

--- script.js
+++ script.js
@@ -20,4 +20,15 @@ document.addEventListener("DOMContentLoaded", function () {
     colorInput.value = ""; // Resets the input field after it's submitted
     boxColor = newBoxColor; // Sets the box color to the new color that's submitted
   });
+
+  function addNewBox() {
+    const box = document.createElement("div");
+    box.setAttribute("data-box-id", boxId.toString()); // Converts boxId to a string and stores it to the data attribute.
+    box.textContent = `Box ${boxId}`; // Displays box ID as text
+    box.className = "box"; // Sets the CSS class to "box"
+    box.style.backgroundColor = boxColor; // Sets the box's background color to the last submitted box color
+    boxContainer.appendChild(box); // Appends the new box to the box container element as it's child
+
+    boxId++; // Increments the counter to keep unique id's for additional new boxes.
+  }
 });
